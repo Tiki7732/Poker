961e1a040cb04c3188d8d079ab1b417350842cfd
tiki7732 <lsaidah@gmail.com>
2020-07-14 14:47:48 -0500

    sperated the return attributes from initialize

--- spec/card_spec.rb
+++ spec/card_spec.rb
@@ -4,14 +4,29 @@ require 'colorize'
 
 describe "Part 1" do
     describe Card do
-        let (:card) {Card.new('🂡', :black)}
+        let (:card) {Card.new('🂡', :black, :spades)}
 
         describe "#initialize" do
-            it "should accept a symbol" do
+            it "should accept a symbol, color, suit" do
+                expect{Card.new('🂡', :black, :spades)}.to_not raise_error
+            end
+        end
+
+        describe "#symbol" do
+            it "should return symbol" do
                 expect(card.symbol).to eq('🂡')
             end
-            it "should accept a color" do
+        end
+
+        describe "#color" do
+            it "should return color" do
                 expect(card.color).to eq(:black)
+            end
+        end
+
+        describe "#suit" do
+            it "should return suit" do
+                expect(card.suit).to eq(:spades)
             end
         end
 
