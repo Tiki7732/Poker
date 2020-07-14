9ca37d502a67b6cd95efad45e000eccebc1e8796
tiki7732 <lsaidah@gmail.com>
2020-07-14 13:45:56 -0500

    starting testing outline

--- spec/deck_spec.rb
+++ spec/deck_spec.rb
@@ -1,2 +1,22 @@
 require 'rspec'
 require 'deck'
+
+describe Deck do
+
+    let (:deck) {Deck.new}
+
+    describe "#initialize" do
+        it "should contain 52 unique cards"
+        it "should have 4 suits"
+    end
+
+    describe "#shuffle" do 
+        it "should suffle cards"
+    end
+
+    describe "#deal" do
+        it "should remove top card from deck"
+    end
+
+
+end
