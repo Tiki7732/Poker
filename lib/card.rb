542af3befa60716e09d3a7ffa0aacc47e7c0d20e
tiki7732 <lsaidah@gmail.com>
2020-07-14 14:08:04 -0500

    initialize and to_s methods

--- lib/card.rb
+++ lib/card.rb
@@ -1,3 +1,13 @@
+require 'colorize'
 class Card
+
+    attr_reader :symbol, :color
+    def initialize(symbol, color)
+        @symbol = symbol
+    end
+
+    def to_s
+        "#{symbol}".colorize(color)
+    end
 
 end
