0eb2e87f77ec47430b0ceebbf74b4a7e848c63c8
tiki7732 <lsaidah@gmail.com>
2020-07-14 15:01:39 -0500

    drying out initialize and shuffle

--- spec/deck_spec.rb
+++ spec/deck_spec.rb
@@ -1,22 +1,29 @@
 require 'rspec'
 require 'deck'
 
-describe Deck do
+describe "Part 2" do
+    describe Deck do
 
-    let (:deck) {Deck.new}
+        let (:deck) {Deck.new}
 
-    describe "#initialize" do
-        it "should contain 52 unique cards"
-        it "should have 4 suits"
+        describe "#initialize" do
+            it "should contain 52 unique cards" do
+                expect(deck.cards).to eq(deck.cards.uniq)
+                expect(deck.cards.length).to eq(52)
+            end
+        end
+
+        describe "#shuffle_cards" do 
+            it "should suffle cards" do
+                deck.shuffle_cards
+                expect(deck.cards).to receive(:shuffle!)
+            end
+        end
+
+        describe "#deal" do
+            it "should remove top card from deck"
+        end
+
+
     end
-
-    describe "#shuffle" do 
-        it "should suffle cards"
-    end
-
-    describe "#deal" do
-        it "should remove top card from deck"
-    end
-
-
 end
