516e081dec4322aafd8a07823056239ca92360f2
tiki7732 <lsaidah@gmail.com>
2020-07-14 18:06:49 -0500

    adding cards

--- lib/deck.rb
+++ lib/deck.rb
@@ -2,6 +2,11 @@ require_relative 'card'
 
 class Deck
 
+    #SPADES = [🂡, 🂢, 🂣, 🂤, 🂥, 🂦, 🂧, 🂨, 🂩, ]
+    CARDS = [:A, :2, :3, :4, :5, :6, :7, :8, 9, :10, :J, :Q, :K]
+    SUITS = [:♠, :♥, :♦, :♣]
+    
+    
     attr_reader :cards
     def initialize
         @cards = []
