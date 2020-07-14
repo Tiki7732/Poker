e294904627cad02e75524d60b29a375949dcdb34
tiki7732 <lsaidah@gmail.com>
2020-07-14 14:08:29 -0500

    adding testing guides

--- spec/card_spec.rb
+++ spec/card_spec.rb
@@ -1,8 +1,17 @@
 require 'rspec'
 require 'card'
 
-describe Card do
-    let (:card) {Card.new}
+describe "Part 1" do
+    describe Card do
+        let (:card) {Card.new}
 
-    
+        describe "#initialize" do
+            it "should accept a symbol"
+            it "should accept a color"
+        end
+
+        describe "#to_s" do
+            it "should puts symbold with specified color"
+        end
+    end
 end
