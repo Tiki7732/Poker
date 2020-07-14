16983bfb5c61e6c283ff768bf43ae3546f8433ec
tiki7732 <lsaidah@gmail.com>
2020-07-14 14:29:44 -0500

    filled out tests

--- spec/card_spec.rb
+++ spec/card_spec.rb
@@ -1,17 +1,24 @@
 require 'rspec'
 require 'card'
+require 'colorize'
 
 describe "Part 1" do
     describe Card do
-        let (:card) {Card.new}
+        let (:card) {Card.new('🂡', :black)}
 
         describe "#initialize" do
-            it "should accept a symbol"
-            it "should accept a color"
+            it "should accept a symbol" do
+                expect(card.symbol).to eq('🂡')
+            end
+            it "should accept a color" do
+                expect(card.color).to eq(:black)
+            end
         end
 
         describe "#to_s" do
-            it "should puts symbold with specified color"
+            it "should puts symbold with specified color" do
+                expect(card.to_s).to eq('🂡'.colorize(:black))
+            end
         end
     end
 end
