cc5e8d031fafda70a96a96184331ddd43efbd9cf
tiki7732 <lsaidah@gmail.com>
2020-07-16 12:08:40 -0500

    added add_card test

--- spec/hand_spec.rb
+++ spec/hand_spec.rb
@@ -4,8 +4,12 @@ require 'hand'
 describe 'Part 3' do
     describe  Hand do
         let (:hand) {Hand.new()}
-        describe '#initialize' do
-            it "should have 5 cards"
+        describe '#add_card' do
+            let(:card){double("card", :symbol => :K)}
+            it "should add a card to the hand" do
+                hand.add_card(card)
+                expect(hand.cards.length).to eq(1)
+            end
 
         end
         
