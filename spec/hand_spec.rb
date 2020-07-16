94dadf04ff1e21f2a9cd3ffa2406fa09d82f806d
tiki7732 <lsaidah@gmail.com>
2020-07-16 11:57:02 -0500

    adding hand testing guides

--- spec/hand_spec.rb
+++ spec/hand_spec.rb
@@ -1,2 +1,20 @@
 require 'rspec'
 require 'hand'
+
+describe 'Part 3' do
+    describe  Hand do
+        let (:hand) {Hand.new()}
+        describe '#initialize' do
+            it "should have 5 cards"
+
+        end
+        
+        describe '#strongest' do
+            it "should show the strongest hand"
+        end
+
+        describe '#winning?' do
+            it "should pick winning hand"
+        end
+    end
+end
