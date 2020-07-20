8cc1d8c94bd0b99eb013c9cfcd8bd904cc016589
tiki7732 <lsaidah@gmail.com>
2020-07-20 14:27:05 -0500

    added subjects and a few more tests

--- spec/hand_spec.rb
+++ spec/hand_spec.rb
@@ -3,69 +3,93 @@ require 'hand'
 
 describe 'Part 3' do
     describe  Hand do
-        let (:hand) {Hand.new()}
+        subject (:one_p) {Hand.new}
         let(:card1){double("card1", :symbol => :K, :suit => :♠)}
         let(:card2){double("card2", :symbol => :K, :suit => :♣)}
         let(:card3){double("card3", :symbol => :Q, :suit => :♣)}
         let(:card4){double("card4", :symbol => 2.to_s.to_sym, :suit => :♦)}
-        let(:card5){double("card5", :symbol => 5.to_s.to_sym, :suit => :♥)}
+        let(:card5){double("card5", :symbol => 5.to_s.to_sym, :suit => :♥)}        
         describe '#add_card' do
             
             it "should add a card to the hand" do
-                hand.add_card(card1)
-                expect(hand.cards.length).to eq(1)
+                one_p.add_card(card1)
+                expect(one_p.cards.length).to eq(1)
             end
 
         end
 
         describe "#rank_card" do
             it "should return rank of card" do
-                hand.add_card(card1)
-                expect(hand.rank_card(card1)).to eq(2)
+                one_p.add_card(card1)
+                expect(one_p.rank_card(card1)).to eq(2)
             end
         end
 
         describe "#highest_card" do
             it "should return rank of highest card" do
-                expect(hand.highest_card([card2,card3])).to eq(2)
+                expect(one_p.highest_card([card2,card3])).to eq(2)
             end
         end
         
         describe '#best_hand' do
             it "should show the highest ranking hand" do
-                hand.add_card(card1)
-                hand.add_card(card2)
-                hand.add_card(card3)
-                hand.add_card(card4)
-                hand.add_card(card5)
-                expect(hand.best_hand).to eq([:ONE_PAIR, card1.to_s, card2.to_s, card3.to_s])
+                one_p.add_card(card1)
+                one_p.add_card(card2)
+                one_p.add_card(card3)
+                one_p.add_card(card4)
+                one_p.add_card(card5)
+                expect(one_p.best_hand).to eq([:ONE_PAIR, card1.to_s, card2.to_s, card3.to_s])
             end
         end
 
         describe "#find_matches" do
             it "should return cards with matching symbols" do
-                hand.add_card(card1)
-                hand.add_card(card2)
-                hand.add_card(card3)
-                hand.add_card(card4)
-                hand.add_card(card5)
-                expect(hand.find_matches).to eq([card1, card2])
+                one_p.add_card(card1)
+                one_p.add_card(card2)
+                one_p.add_card(card3)
+                one_p.add_card(card4)
+                one_p.add_card(card5)
+                expect(one_p.find_matches).to eq([card1, card2])
+            end
+        end
+        subject(:straight_flus) {Hand.new}
+        let(:card6){double("card1", :symbol => :A, :suit => :♣)}
+        let(:card7){double("card2", :symbol => :K, :suit => :♣)}
+        let(:card8){double("card3", :symbol => :Q, :suit => :♣)}
+        let(:card9){double("card4", :symbol => :J, :suit => :♣)}
+        let(:card10){double("card5", :symbol => 10.to_s.to_sym, :suit => :♣)}
+
+        describe "#rank_hand" do
+            it "should return rank of best hand" do
+                one_p.add_card(card1)
+                one_p.add_card(card2)
+                one_p.add_card(card3)
+                one_p.add_card(card4)
+                one_p.add_card(card5)
+                straight_flus.add_card(card6)
+                straight_flus.add_card(card7)
+                straight_flus.add_card(card8)
+                straight_flus.add_card(card9)
+                straight_flus.add_card(card10)
+                expect(straight_flus.rank_hand).to eq(1)
+                expect(one_p.rank_hand).to eq(9)
             end
         end
 
-        describe "#rank_hand" do
-        let(:card1){double("card1", :symbol => :A, :suit => :♣)}
-        let(:card2){double("card2", :symbol => :K, :suit => :♣)}
-        let(:card3){double("card3", :symbol => :Q, :suit => :♣)}
-        let(:card4){double("card4", :symbol => :J, :suit => :♣)}
-        let(:card5){double("card5", :symbol => 10.to_s.to_sym, :suit => :♣)}
-            it "should return rank of best hand" do
-                hand.add_card(card1)
-                hand.add_card(card2)
-                hand.add_card(card3)
-                hand.add_card(card4)
-                hand.add_card(card5)
-                expect(hand.rank_hand).to eq(1)
+        describe "#straight?" do
+            it "should return true if the hand is a straight" do
+                one_p.add_card(card1)
+                one_p.add_card(card2)
+                one_p.add_card(card3)
+                one_p.add_card(card4)
+                one_p.add_card(card5)
+                straight_flus.add_card(card6)
+                straight_flus.add_card(card7)
+                straight_flus.add_card(card8)
+                straight_flus.add_card(card9)
+                straight_flus.add_card(card10)
+                expect(straight_flus.straight?).to eq(true)
+                expect(one_p.straight?).to eq(false)
             end
         end
 
