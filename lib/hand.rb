06c58fbc218e8bfc94c326123f2aea2b954bdeb1
tiki7732 <lsaidah@gmail.com>
2020-07-20 13:17:25 -0500

    added highest_card method and reworking best_hand and rank_hand

--- lib/hand.rb
+++ lib/hand.rb
@@ -1,16 +1,16 @@
 require_relative 'card'
 
 class Hand
-    HAND_RANKINGS = {:ROYAL_FLUSH => 0,
-                :STRAIGHT_FLUSH => 1,
-                :FOUR_OF_A_KIND => 2,
-                :FULL_HOUSE => 3,
-                :FLUSH => 4,
-                :STRAIGHT => 5,
-                :THREE_OF_A_KIND => 6,
-                :TWO_PAIR => 7,
-                :ONE_PAIR => 8,
-                :HIGH_CARD =>9}
+    HAND_RANKINGS = {:ROYAL_FLUSH => 1,
+                :STRAIGHT_FLUSH => 2,
+                :FOUR_OF_A_KIND => 3,
+                :FULL_HOUSE => 4,
+                :FLUSH => 5,
+                :STRAIGHT => 6,
+                :THREE_OF_A_KIND => 7,
+                :TWO_PAIR => 8,
+                :ONE_PAIR => 9,
+                :HIGH_CARD => 10}
     CARD_RANKINGS = {:A => 1, :K => 2, :Q => 3, :J => 4, :'10' => 5, :'9' => 6, 
                     :'8' => 7, :'7' => 8, :'6' => 9, :'5' => 10, :'4' => 11, 
                     :'3' => 12, :'2' => 13}
@@ -25,21 +25,36 @@ class Hand
     end
 
     def best_hand
+        case cards.rank_hand
+        when 1
+            return [:ROYAL_FLUSH, ]
+        when :ONE_PAIR
+            return [:ONE_PAIR, ]
+        end
+    end
 
+    def highest_card(cards_arr)
+        rank = 20
+        cards_arr.each {|card| rank = rank_card(card) if rank_card(card) < rank}
+        rank
     end
 
     def rank_card(card)
-        rank = CARD_RANKINGS[card.symbol] 
+        CARD_RANKINGS[card.symbol] 
     end
 
     def rank_hand
-        case cards
-        when cards.all?{|card| card.suit == cards.first.suit} &&
-            cards.all?{|card| rank_card(card) <= 5}
-            return :ROYAL_FLUSH
-        else
-            return :HIGH_CARD
-        end
+        # case cards
+        # when cards.each {|card| p rank_card(card)} 
+        #     p cards.length
+        #     same_suit = cards.all? { |card| card.suit == cards.first.suit}
+        #     p same_suit
+        #     #&&
+        #     #cards.all?{|card| rank_card(card) <= 5}
+        #     return :ROYAL_FLUSH
+        # else
+        #     return :HIGH_CARD
+        # end
     end
 
 end
