cdd1236423ee0af290a5daf5105a87b0d69aebfe
tiki7732 <lsaidah@gmail.com>
2020-07-27 12:05:32 -0500

    added receive_Card and bet methods

--- lib/player.rb
+++ lib/player.rb
@@ -7,4 +7,20 @@ class Player
         @hand = Hand.new
         @pot = 1000
     end
+
+    def receive_card(card)
+        hand.add_card(card)
+    end
+
+    def bet
+        amount = nil
+        until amount && amount.between?(1, pot)
+            p "The minimum bet is $1"
+            amount = gets.chomp.to_i
+        end
+        amount
+    end
 end
+
+# lt = Player.new
+# p lt.bet
