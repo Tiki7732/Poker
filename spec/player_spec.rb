8fa2832ba1a9ca9b6e690778a19ce2b9a8d4e245
tiki7732 <lsaidah@gmail.com>
2020-07-27 13:46:06 -0500

    added discard test

--- spec/player_spec.rb
+++ spec/player_spec.rb
@@ -16,14 +16,21 @@ describe "Part 4" do
         describe "#bet" do
             it "should place a bet between 0 and total pot of player" do
                 expect(player1.bet).to be_between(1, player1.pot)
-                #expect(player1.bet).to be <= (player1.pot)
             end
         end
 
         describe "#discard" do
-            it "should remove up to 4 cards from players hand"
-
-            it "should draw an equal number of new cards from deck"
+        let(:card1){double("card1", :symbol => :K, :suit => :♠)}
+        let(:card2){double("card2", :symbol => :K, :suit => :♣)}
+        let(:card3){double("card3", :symbol => :Q, :suit => :♣)}
+        let(:card4){double("card4", :symbol => 2.to_s.to_sym, :suit => :♦)}
+        let(:card5){double("card5", :symbol => 5.to_s.to_sym, :suit => :♥)} 
+        let(:hand1){[card1, card2, card3, card4, card5]} 
+            it "should remove up to 4 cards from players hand" do
+                player1.hand.add_cards(hand1)
+                player1.discard
+                expect(player1.hand.cards.length).to be < 5
+            end
         end
     end
 end
