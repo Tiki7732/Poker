b5b4c8cc7ced5629d2a52741963f33017097122f
tiki7732 <lsaidah@gmail.com>
2020-07-27 11:41:00 -0500

    added intialize testing

--- spec/player_spec.rb
+++ spec/player_spec.rb
@@ -4,5 +4,13 @@ require 'player'
 describe "Part 4" do
     describe Player do
         let(:player1){Player.new}
+        describe "#initialize" do
+            it "should initialize player with a pot" do
+                expect(player1.pot).to be > 0
+            end
+            it "should initialize with an empty hand" do
+                expect(player1.hand.cards.length).to equal(0)
+            end
+        end
     end
 end
