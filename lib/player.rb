04fab6a7a289a092efd3d98b09117a9d0dea6b02
tiki7732 <lsaidah@gmail.com>
2020-07-28 12:45:36 -0500

    reworked bet method and added get_amount helper method

--- lib/player.rb
+++ lib/player.rb
@@ -20,23 +20,55 @@ class Player
         discard = gets.chomp
         case discard
         when "none"
-            return
+            return nil
         else
             cards = discard.split("").map!(&:to_i)
             hand.remove_card(cards)
+            return cards.length
         end
 
     end
 
-    def bet
-        amount = nil
-        until amount && amount.between?(1, pot)
-            p "The minimum bet is $1"
-            amount = gets.chomp.to_i
+    def bet(minimum)
+        if minimum > pot
+            p "You do not have enough to bet"
+            return nil
         end
-        amount
+
+        p "The minimum bet is #{minimum}, you can fold, see, or raise"
+        action = gets.chomp
+
+        case action
+        when 'fold'
+            return nil
+        when 'see'
+            @pot -= minimum
+            return minimum
+        when 'raise'
+            amount = get_amount(minimum)
+            p "All in!" if amount == pot
+            @pot -= amount
+            return amount
+        end
+        
+      
+    end
+
+    def get_amount(minimum)
+        amount = nil
+        p "You currently have #{pot - minimum} to raise with"
+        p "How much would you like to raise?"
+        until amount && amount <= (pot - minimum)
+            amount = gets.chomp.to_i
+            if amount > (pot - minimum)
+                amount = nil
+                p "You do not have enough in your pot, you can raise up to #{pot - minimum}"
+                next
+            end
+            return amount + minimum
+        end
     end
 end
 
 # lt = Player.new
-# p lt.bet
+# p lt.bet(5)
