a78334a47d3c67e1075fa3efb89710cd59f14ffd
tiki7732 <lsaidah@gmail.com>
2020-07-30 13:58:21 -0500

    added deal_cards method

--- lib/game.rb
+++ lib/game.rb
@@ -10,4 +10,12 @@ class Game
         4.times {@players.push(Player.new)}
     end
         
+    def deal_cards
+        3.times {deck.shuffle_cards}
+        5.times do 
+            players.each do |player|
+                player.receive_card(deck.deal)
+            end
+        end
+    end
 end
