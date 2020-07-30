d12c7b0f61f00234f6cf3700a125182b7c178fc4
tiki7732 <lsaidah@gmail.com>
2020-07-30 16:31:51 -0500

    dryed out get_bet and ante by added folded? method

--- lib/game.rb
+++ lib/game.rb
@@ -3,13 +3,15 @@ require_relative 'player'
 
 class Game
 
-    attr_reader :deck, :players, :current_player, :pot
+    attr_reader :deck, :players, :current_player, :pot, :minimum, :folded_players
     def initialize
         @deck = Deck.new
         @players = []
         4.times {@players.push(Player.new)}
         @current_player = @players.first
         @pot = 0
+        @minimum = 2
+        @folded_players = []
     end
         
     def deal_cards
@@ -33,5 +35,21 @@ class Game
 
     def pot_amount?
         @pot
+    end
+
+    def get_bet
+        bet = @current_player.bet(minimum)
+        folded?(bet)
+        @pot += bet
+    end
+
+    def folded?(bet)
+        @folded_players.push(@players.delete(@current_player)) if bet.nil?
+    end
+
+    def ante
+        ante = @current_player.ante_up(@minimum)
+        folded?(ante)
+        @pot += ante
     end
 end
