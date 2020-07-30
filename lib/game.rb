55aa27ed7b5dae9fd6d13a8307c31b6b6309f7b0
tiki7732 <lsaidah@gmail.com>
2020-07-30 11:51:02 -0500

    defined class and initialize

--- lib/game.rb
+++ lib/game.rb
@@ -1,2 +1,13 @@
 require_relative 'deck'
 require_relative 'player'
+
+class Game
+
+    attr_reader :deck, :players
+    def initialize
+        @deck = Deck.new
+        @players = []
+        4.times {@players.push(Player.new)}
+    end
+        
+end
